e2c674e7c33e9ec873721be09ccc42c8f229cc8c
Arthur <arthur.richards@bristol.ac.uk>
2020-10-20 14:55:57 +0100

    Revert "Attempting simpler Dockerfile through use of COPY, but failing."
    This reverts commit bcddc3683806d70e5a7386e9a71bf75d65ee0749.

--- ardupilot/multi/docker/sitl_copter/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash' > launch.sh
-/ardupilot/build/sitl/bin/arducopter --model=quad --defaults=/ardupilot/Tools/autotest/default_params/copter.parm --sysid=$SYSID &'
-mavproxy.py --non-interactive --target-system=$SYSID --master=tcp:0.0.0.0:5760 --out=udp:172.17.0.1:14553'
